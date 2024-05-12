f3cba17f290486904b5e025296f4fe928d854cc0
Daniel Lucas <hi@danlgz.io>
2024-05-12 00:06:37 -0600

    feat: bar color

--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "@remix-run/react";
 export default function SlugIndexRoute() {
   return (
     <>
-      <nav className="w-full max-w-screen-md mx-auto my-4 py-4 flex flex-row items-center justify-between border-b border-background/30">
+      <nav className="w-full max-w-screen-md mx-auto my-4 py-4 flex flex-row items-center justify-between border-b border-main/30 dark:border-main-dark/30">
         <Link to="..">
           <h3>Daniel Lucas – Blog</h3>
         </Link>
